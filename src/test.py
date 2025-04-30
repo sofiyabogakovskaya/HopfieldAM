10ea27d8c6e5503f5701c07999fb95805400320f
soneykaa <bogakovskaia.siu@phystech.edu>
2025-04-30 17:00:45 +0300

    fixed a bug 1.0

--- src/test.py
+++ src/test.py
@@ -4,4 +4,4 @@ from utils.metrics import batch_accuracy
 def test(model, test_loader, dt, t1, N_classes):
     test_accuracy = batch_accuracy(model, test_loader, dt, t1, N_classes)
     print(f"Test Accuracy: {test_accuracy:.2%}")
-    return test_accuracy
+    return float(test_accuracy)
