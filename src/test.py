8be96c4052f1fa66afa675e39329b5fd63ad18f7
soneykaa <bogakovskaia.siu@phystech.edu>
2025-05-02 02:14:59 +0300

    quick changes

--- src/test.py
+++ src/test.py
@@ -9,6 +9,8 @@ def test(model, test_loader, batch_loss, dt, t1, N_classes):
         total_loss += loss_value
         num_batches += 1   
     test_loss = total_loss / num_batches
+    test_loss = round(float(test_loss), 4)
     test_accuracy = batch_accuracy(model, test_loader, dt, t1, N_classes)
+    test_accuracy = round(float(test_accuracy), 4)
     print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy:.2%}")
-    return round(float(test_loss), 5), round(float(test_accuracy), 5)
+    return test_loss, test_accuracy
--- src/train.py
+++ src/train.py
@@ -49,13 +49,16 @@ def train(model,
                                                   )
             total_loss += loss_value
             num_batches += 1   
-        avg_loss = total_loss / num_batches
-        val_acc = batch_accuracy(model, val_loader, dt, t1, N_classes)  
+        val_loss = total_loss / num_batches
+        val_loss = round(float(val_loss), 5)
+        val_accuracy = batch_accuracy(model, val_loader, dt, t1, N_classes)  
+        val_accuracy = round(float(val_accuracy), 5)
 
-        print(f"Epoch {epoch+1}/{epochs} - Loss: {avg_loss:.4f} - Val Accuracy: {val_acc:.2%}")
+        
+        print(f"Epoch {epoch+1}/{epochs} - Loss: {val_loss:.4f} - Val Accuracy: {val_accuracy:.2%}")
 
-        val_losses.append(round(float(avg_loss), 5))
-        val_accuracies.append(round(float(val_acc), 5)) 
+        val_losses.append(val_loss)
+        val_accuracies.append(val_accuracy) 
 
         # log_message(f"Epoch {epoch+1}/{epochs} - Loss: {avg_loss:.4f} - Val Accuracy: {val_acc:.2%}")
         # log_metrics({"epoch": epoch + 1, "loss": float(avg_loss), "val_accuracy": float(val_acc)})
