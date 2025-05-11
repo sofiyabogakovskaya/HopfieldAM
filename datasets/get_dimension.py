ac6a7a9115b130ee98785a0064459ce1293f7fc2
sofiyabogakovskaya <s.bogakovskaya@gmail.com>
2025-05-11 21:56:05 +0300

    tried to fix again

--- datasets/get_dimension.py
+++ datasets/get_dimension.py
@@ -5,5 +5,8 @@ def get_dimension(dataset_name):
     """returns the flattened input dimension for the given dataset."""
     ds = tfds.load(dataset_name, split="train", batch_size=1)
     example = next(iter(tfds.as_numpy(ds)))
-    x, _ = example
-    return int(np.prod(x.shape[1:]))  # skip batch dimension
+    x = example["image"] 
+    return int(np.prod(x.shape[1:]))  # skip batch 
+
+
+
