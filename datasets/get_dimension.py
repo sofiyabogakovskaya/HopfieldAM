74f6cef5e76b52548ce03833ca19c6470c728819
sofiyabogakovskaya <s.bogakovskaya@gmail.com>
2025-05-11 21:48:31 +0300

    fixed an error

--- datasets/get_dimension.py
+++ datasets/get_dimension.py
@@ -4,6 +4,6 @@ import tensorflow_datasets as tfds
 def get_dimension(dataset_name):
     """returns the flattened input dimension for the given dataset."""
     ds = tfds.load(dataset_name, split="train", batch_size=1)
-    example = next(tfds.as_numpy(ds))
+    example = next(iter(tfds.as_numpy(ds)))
     x, _ = example
     return int(np.prod(x.shape[1:]))  # skip batch dimension
