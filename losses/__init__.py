263fcafea575bb03940840572f4542cedc490774
sofiyabogakovskaya <s.bogakovskaya@gmail.com>
2025-10-29 14:34:09 +0300

    relocated config and activation map

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,8 +1,7 @@
 from .loss_last10 import batch_loss_last10
 
-
 def get_batch_loss(name):
     losses = {
-        "batch_loss_last10": batch_loss_last10
+        'batch_loss_last10': batch_loss_last10
     }
-    return losses.get(name, batch_loss_last10)  
+    return losses.get(name, batch_loss_last10)
