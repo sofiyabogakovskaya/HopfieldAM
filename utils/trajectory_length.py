c7419352643f02e75d5ad4f943a3f1dd635a613a
sofiyabogakovskaya <s.bogakovskaya@gmail.com>
2025-11-06 03:48:11 +0300

    more functions to analyze trajectory

--- utils/trajectory_length.py
+++ utils/trajectory_length.py
@@ -3,5 +3,15 @@ import jax.numpy as jnp
 def trajectory_length(x):
     diffs = jnp.diff(x, axis=0)
     step_norms = jnp.linalg.norm(diffs, axis=-1)
-    lengths = jnp.sum(step_norms, axis=0)
-    return lengths
+    length = jnp.sum(step_norms, axis=0)
+    return length 
+
+def trajectory_direction(x):
+    x_0 = x[0]
+    x_T = x[-1]
+    dist_vec = jnp.linalg.norm(x_T - x_0)
+    dist = jnp.sum(dist_vec)
+    return dist
+
+def how_straight(traj_length, traj_dir):
+    return traj_length - traj_dir
